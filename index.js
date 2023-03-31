0769b2a90429b68f3825e994c467c28435367b1b
Zemorath <trentward10@gmail.com>
2023-03-30 22:53:37 -0500

    Lab on Loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names) {
+    const namesWithMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        namesWithMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return namesWithMessages;
+}
+
+function countDown(n) {
+    let i = 0;
+    while (i <= n) {
+        console.log(n)
+        n--;
+    }
+}
